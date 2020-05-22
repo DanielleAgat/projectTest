2e4142885879978deadff08563842a5020faf0a3
DanielleAgat <agatlevi96@gmail.com>
2020-05-22 10:54:22 +0300

    deleted redundant spaces

--- main.c
+++ main.c
@@ -15,8 +15,7 @@ void main()
 	boardPosArray** positionsArray;
 	int i, j;
 	moves = (movesArray**)malloc(sizeof(movesArray*) * N);
-	for (i = 0; i < N; i++)
-	{
+	for (i = 0; i < N; i++){
 		moves[i] = (movesArray*)malloc(sizeof(movesArray) * M);
 	}
 
@@ -27,18 +26,14 @@ void main()
 	printf("\n");
 	positionsArray = validMoves(moves, board);
 
-	for (i = 0; i < N; i++)
-	{
-		for (j = 0; j < M; j++)
-		{
+	for (i = 0; i < N; i++){
+		for (j = 0; j < M; j++){
 			int indexPos = 0;
-			while (indexPos < positionsArray[i][j].size)
-			{
+			while (indexPos < positionsArray[i][j].size){
 				printf("The position for %c%c is:%c%c\n", i + 'A', j + '1', positionsArray[i][j].positions[indexPos][0], positionsArray[i][j].positions[indexPos][1]);
 				printf("\n");
 				indexPos++;
 			}
-
 		}
 	}
 }
