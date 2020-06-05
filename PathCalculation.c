9fdcaaa90b142a841b3bc14fabe05d764eac0222
DanielleAgat <agatlevi96@gmail.com>
2020-06-05 16:55:57 +0300

    Create file for question 4

---
+++ PathCalculation.c
@@ -0,0 +1,7 @@
+//Student's name 1: Danielle Agat Levi
+//Student's ID 1: 313357329
+//Student's name 2: Tal Luvton
+//Student's ID 2: 318420601
+
+
+#include "PathCalculation.h"
---
+++ PathCalculation.h
@@ -0,0 +1,17 @@
+//Student's name 1: Danielle Agat Levi
+//Student's ID 1: 313357329
+//Student's name 2: Tal Luvton
+//Student's ID 2: 318420601
+
+#ifndef PROJECT_PATHCALCULATION_H
+#define PROJECT_PATHCALCULATION_H
+#include "MovesArray.h"
+#include "MovesList.h"
+#include "List_Tests.h"
+#include "Types.h"
+#include "tree.h"
+
+
+
+
+#endif
